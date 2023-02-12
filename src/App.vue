dd3aa7770e49b20190dda85ce0d22ff0d3ad8b7c
Delfx <motiejunas.lukas@gmail.com>
2023-02-12 22:48:29 +0200

    added new component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <h1 class="col-md-6"> Test </h1>
+
+  <div class="presentation col-6">
+    <Presentation class="m-2 d-flex align-items-center" />
+  </div>
 
   <Renderer :pointer="{ intersectRecursive: true }" ref="rendererC" antialias
-    :orbit-ctrl="{ enableDamping: false, enabled: false }" resize="window">
+    :orbit-ctrl="{ enableDamping: true, enabled: false }" resize="window">
 
     <Camera ref="camera" :position="{ z: 5, x: 1, y: 1 }" />
 
     <Scene ref="sceneC" background="#F2CD5C">
       <AmbientLight color="white" :intensity="0.2" :position="{ z: 400 }" />
-      <PointLight ref="pointLightBlue" :position="{ z: propertyY, x: propertyX }" color="blue" :intensity="1" />
+      <PointLight :position="{ z: 10, x: 100 }" :color="lightColorRight" :intensity="1" />
       <PointLight :position="{ z: 10, x: 10 }" color="blue" :intensity="0.5" />
-      <PointLight :position="{ z: 50, x: -80 }" color="red" :intensity="0.6" />
+      <PointLight :position="{ z: 50, x: -80 }" :color="lightColorLeft" :intensity="0.6" />
       <PointLight :position="{ z: -50, x: -50 }" color="white" :intensity="0.3" />
       <PointLight :position="{ z: 80, x: 80 }" color="white" :intensity="0.1" />
 
-      <GltfModel @click="onClick" :position="{x: 1, y:0.3, z:1}" :rotation="{y: modelRotationY}"  ref="model" src="/galva2.gltf">
+      <GltfModel @click="onClick" :position="{ x: 1, y: 0.6, z: 1 }" :rotation="{ y: modelRotationY }" ref="model"
+        src="/galva2.gltf">
       </GltfModel>
 
     </Scene>
@@ -23,26 +27,30 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, onBeforeMount } from 'vue'
-import { Box, Camera, PointLight, Renderer, Scene, Texture, AmbientLight, GltfModel, MeshPublicInterface, RendererPublicInterface, LambertMaterial, } from 'troisjs'
-import { Mesh, PerspectiveCamera } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { ref, onMounted } from 'vue'
+import { Camera, PointLight, Renderer, Scene, AmbientLight, GltfModel, RendererPublicInterface } from 'troisjs'
+import Presentation from './components/Presentation.vue'
+
 
 const rendererC = ref()
 const camera = ref()
 const meshC = ref()
 const model = ref()
 const sceneC = ref()
-const pointLightBlue = ref()
-const propertyY = ref(10)
-const propertyX = ref(100)
 const modelRotationY = ref(0)
+const lightColorRight = ref('blue');
+const lightColorLeft = ref('red');
 
 const winSize = () => {
   return {
     width: window.innerWidth,
     height: window.innerHeight
   }
+}
+
+//create random color generator
+const randomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 const winSizeCheck = () => {
@@ -54,53 +62,17 @@ const winSizeCheck = () => {
 }
 
 const onClick = (event: any) => {
-    // console.log(event);
-    modelRotationY.value += 0.1
-  }
+  // console.log(randomColor());
+  lightColorRight.value = randomColor();
+  lightColorLeft.value = randomColor();
+  modelRotationY.value = 0
+  // modelRotationY.value += 0.2
+}
 
 onMounted(() => {
-
-  // winSizeCheck()
-  // model.value.position.z = 1
-  // model.value.position.x = 1.2
-  // model.value.position.y = 0.3
-  // model.value.rotation.y = -0.4
-  //get win size with vue3
-
-
-
-//create onclikc event for model
-  
-
-    // console.log('click');
-    
-    // propertyY.value += 100
-  
-
-
-
-
-  //if winsize equal or lower than 768px then set camera position to 0,0,0
-
-
-
-
-
-  // console.log(meshC.value);
-
-  // model.value.geometry.center()
-  // model.value.scene.position.x = 2
-  // console.log(model.value);
-
-
-  // model.value?.setRotationFromQuaternion(new Quaternion(10, 0, 10, 1))
-  // const modelC = model.value as ConvexGeometry
-  // modelC.convex
-
-
   const renderer = rendererC.value as RendererPublicInterface
   renderer.onBeforeRender(() => {
-// modelRotationY.value += 0.01  
+    modelRotationY.value += 0.002
   })
 })
 
@@ -111,14 +83,15 @@ onMounted(() => {
 body,
 html {
   margin: 0;
-  /* height: 100%; */
+}
+
+.presentation {
+  margin-top: 13  .5rem;
 }
 
 h1 {
   z-index: 1;
-  /* color: white; */
 }
-
 
 canvas {
   width: 100%;
---
+++ src/components/Presentation.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="">Lukas Motiejūnas</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
