fcd699db4c493d7ebe6614053e3dceabf2f71ebd
Delfx <motiejunas.lukas@gmail.com>
2023-02-22 17:30:46 +0200

    added small descriptions

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -13,54 +13,58 @@
             the ever-evolving world of technology.</h6>
     </div>
 
-    <div class="mt-3">
-        <a class="row text-decoration-none" href="https://github.com/Delfx" target="_blank">
-            <div class="col-md-1 col-3">
-                <i style="font-size: 2rem;" class="bi bi-github text-black"></i>
-            </div>
+    <div class="logo mt-3">
+        <div>
+            <a class="row text-decoration-none" href="https://github.com/Delfx" target="_blank">
+                <div class="col-md-1 col-3">
+                    <i style="font-size: 2rem;" class="bi bi-github text-black"></i>
+                </div>
 
-            <div class="col-2 align-self-center">
-                <h6 class="m-0 text-black">github.com/Delfx</h6>
-            </div>
-        </a>
-    </div>
+                <div class="col-2 align-self-center">
+                    <h6 class="m-0 text-black">github.com/Delfx</h6>
+                </div>
+            </a>
+        </div>
 
-    <div class="mt-1">
-        <a class="row text-decoration-none" href="https://www.lukmoti.lt" target="_blank">
-            <div class="col-md-1 col-3">
-                <i style="font-size: 2rem;" class="bi bi-globe2 text-black"></i>
-            </div>
+        <div class="mt-1">
+            <a class="row text-decoration-none" href="https://www.lukmoti.lt" target="_blank">
+                <div class="col-md-1 col-3">
+                    <i style="font-size: 2rem;" class="bi bi-globe2 text-black"></i>
+                </div>
 
-            <div class="col-2 align-self-center">
-                <h6 class="m-0 text-black">www.lukmoti.lt</h6>
-            </div>
-        </a>
-    </div>
+                <div class="col-2 align-self-center">
+                    <h6 class="m-0 text-black">www.lukmoti.lt</h6>
+                </div>
+            </a>
+        </div>
 
-    <div class="mt-1 row">
-        <a class="col-md-1 col-3 text-decoration-none" href="mailto: home@lukmoti.lt" target="_blank">
-            <div class="">
-                <i style="font-size: 2rem;" class="bi bi-envelope text-black"></i>
-            </div>
-        </a>
+        <div class="mt-1 row">
+            <a class="col-md-1 col-3 text-decoration-none" href="mailto: home@lukmoti.lt" target="_blank">
+                <div class="">
+                    <i style="font-size: 2rem;" class="bi bi-envelope text-black"></i>
+                </div>
+            </a>
             <div class="col-2 align-self-center">
                 <h6 class="m-0 text-black">home@lukmoti.lt</h6>
             </div>
 
+        </div>
+
+        <div class="mt-1">
+            <a class="row text-decoration-none" href="https://www.linkedin.com/in/lukas-motiejūnas-5b097753"
+                target="_blank">
+                <div class="col-md-1 col-3">
+                    <i style="font-size: 2rem;" class="bi bi-linkedin text-black"></i>
+                </div>
+
+                <div class="col-2 align-self-center">
+                    <h6 class="m-0 text-black">linkedin.com/lukasMotiejūnas</h6>
+                </div>
+            </a>
+        </div>
     </div>
 
-    <div class="mt-1">
-        <a class="row text-decoration-none" href="https://www.linkedin.com/in/lukas-motiejūnas-5b097753" target="_blank">
-            <div class="col-md-1 col-3">
-                <i style="font-size: 2rem;" class="bi bi-linkedin text-black"></i>
-            </div>
-
-            <div class="col-2 align-self-center">
-                <h6 class="m-0 text-black">linkedin.com/lukasMotiejūnas</h6>
-            </div>
-        </a>
-    </div>
-
+    <h6 class="mt-3 fw-lighter fst-italic">*Click on my Head to change color</h6>
 </template>
 
 <script setup lang="ts">
@@ -68,3 +72,4 @@
 </script>
 
 <style></style>
+
